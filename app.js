6b021d92b6544277af8e39431975c3501bdbbe5d
Iyán Sanjurjo <71704577+UO264921@users.noreply.github.com>
2021-03-30 23:38:00 +0200

    SDI-2021-813-8.5-Colecciones relacionadas y routers.

--- app.js
+++ app.js
@@ -9,15 +9,53 @@ let gestorBD = require("./modules/gestorBD.js");
 let crypto = require('crypto');
 let expressSession = require('express-session');
 
-app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload());
-app.use(expressSession({ secret: 'abcdefg', resave: true, saveUninitialized: true }));
-app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.essby.mongodb.net:27017,tiendamusica-shard-00-01.essby.mongodb.net:27017,tiendamusica-shard-00-02.essby.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-u3t42f-shard-0&authSource=admin&retryWrites=true&w=majority');
-app.set('clave','abcdefg');
-app.set('crypto',crypto);
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.essby.mongodb.net:27017,tiendamusica-shard-00-01.essby.mongodb.net:27017,tiendamusica-shard-00-02.essby.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-u3t42f-shard-0&authSource=admin&retryWrites=true&w=majority');
+app.set('clave', 'abcdefg');
+app.set('crypto', crypto);
 gestorBD.init(app, mongodb);
+
+var routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function (req, res, next) {
+    console.log("routerUsuarioSession");
+    if (req.session.usuario) {
+        next();
+    } else {
+        console.log("va a : " + req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+
+//Aplicar routerUsuarioSession
+app.use("/canciones/agregar", routerUsuarioSession);
+app.use("/publicaciones", routerUsuarioSession);
+
+//routerAudios
+let routerAudios = express.Router();
+routerAudios.use(function (req, res, next) {
+    console.log("routerAudios");
+    let path = require('path');
+    let idCancion = path.basename(req.originalUrl, '.mp3');
+    gestorBD.obtenerCanciones({"_id": mongodb.ObjectID(idCancion)}, function (canciones) {
+        if (req.session.usuario && canciones[0].autor == req.session.usuario) {
+            next();
+        } else {
+            res.redirect("/tienda");
+        }
+    })
+});
+
+//Aplicar routerAudios
+app.use("/audios/", routerAudios);
+
+app.use(express.static('public'));
 
 //Variables
 app.set('port', 8081);
@@ -27,8 +65,7 @@ require("./routes/rusuarios.js")(app, swig, gestorBD);
 require("./routes/rcanciones.js")(app, swig, gestorBD);
 require("./routes/rautores.js")(app, swig);
 
-
 //Lanzamiento del servidor
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
     console.log('Servidor activo');
 });
