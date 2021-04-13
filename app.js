d27cded23eae10e026b7020f2bf28e06d2fa147c
Iyán Sanjurjo <71704577+UO264921@users.noreply.github.com>
2021-04-13 12:57:12 +0200

    SDI-2021-813-10.3-SW Cliente JQuery + AJAX.

--- app.js
+++ app.js
@@ -12,7 +12,14 @@ let fs = require('fs');
 let https = require('https');
 let jwt = require('jsonwebtoken');
 
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+    // Debemos especificar todas las headers que se aceptan. Content-Type , token
+    next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(fileUpload());
